6dd0691ddc198cb539d6cf4f286795218d57291b
Hakeem Alavi <103939681+HakeemAlavi@users.noreply.github.com>
2023-06-02 16:46:09 +0300

    Added an idle animation and the controller
    It switches between states 0 and 1 for idle and move respectively

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    Animator animator;
     [SerializeField] float movementSpeed = 6f;
     [SerializeField] float jumpForce = 5f;
 
@@ -18,21 +19,38 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = gameObject.GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
+        if (!Input.anyKey)
+        {
+            animator.SetInteger("state",0);
+        }
+
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             Jump();
+            animator.SetInteger("state",1);
         }
-
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            animator.SetInteger("state",1);
+        }
+    
+        if (Input.GetButtonDown("Vertical"))
+        {
+            animator.SetInteger("state",1);
+        }
     }
 
     void Jump()
